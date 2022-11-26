64b788fcf1348c654d920750a56bbc77dc3e6dad
Ulad Kasach <uladkasach@gmail.com>
2022-11-26 10:47:44 -0500

    feat(checks): define the hasMetadata type check function

--- src/checks/hasMetadata.test.ts
+++ src/checks/hasMetadata.test.ts
@@ -1,9 +1,19 @@
+import { hasMetadata } from '..';
 import { HasMetadata } from './hasMetadata';
+
+describe('HasMetadata', () => {
+  it('should enable accessing an optional metadata property of a type', () => {
+    type FlowerPot = { id?: number; age: number };
+    const pot: HasMetadata<FlowerPot> = { id: 123, age: 72 };
+    expect(pot.id.toFixed).toBeDefined(); // no type errors
+  });
+});
 
 describe('hasMetadata', () => {
   it('should enable accessing an optional metadata property of a type', () => {
     type FlowerPot = { id?: number; age: number };
-    const pot: HasMetadata<FlowerPot> = { id: 123, age: 72 };
-    expect(pot.id).toEqual(123); // no type errors
+    const pot: FlowerPot = { id: 123, age: 72 };
+    if (!hasMetadata(pot, ['id'])) throw new Error('doesnt have metadata');
+    expect(pot.id.toFixed).toBeDefined();
   });
 });
--- src/checks/hasMetadata.ts
+++ src/checks/hasMetadata.ts
@@ -1,7 +1,11 @@
 /**
  * these are the keys most commonly used metadata keys
  */
-type TypicalMetadataKeys = 'id' | 'uuid' | 'createdAt' | 'updatedAt' | 'effectiveAt';
+const typicalMetadataKeys = ['id', 'uuid', 'createdAt', 'updatedAt', 'effectiveAt'] as const;
+/**
+ * these are the keys most commonly used metadata keys
+ */
+type TypicalMetadataKeys = typeof typicalMetadataKeys[number];
 
 /**
  * asserts that any optional metadata keys of the object will now be required
@@ -16,7 +20,7 @@ type TypicalMetadataKeys = 'id' | 'uuid' | 'createdAt' | 'updatedAt' | 'effectiv
  * note:
  * - this type does not make any other optional, non-metadata keys required
  *   - i.e., any optional types on the object that are not specified as metadata keys will not be made required
- * - this type does not _add_ any database-generated properties to the type definition
+ * - this type does not _add_ any metadata properties to the type definition
  *   - i.e., if a metadata key was not already present on the object's type - it will not be added
  */
 export type HasMetadata<T extends Record<string, any>, MetadataKeys = TypicalMetadataKeys> = T &
@@ -28,3 +32,20 @@ export type HasMetadata<T extends Record<string, any>, MetadataKeys = TypicalMet
         MetadataKeys // and are metadata keys
     >
   >;
+
+/**
+ * for an object of type T, asserts that the object HasMetadata<T, K> for all listed metadata keys K
+ *
+ * by default, the following keys are considered to be metadata:
+ * - `id`
+ * - `uuid`
+ * - `createdAt`
+ * - `updatedAt`
+ * - `effectiveAt`
+ *
+ * see more information about the `HasMetadata` type in its jsdoc on-hover documentation
+ */
+export const hasMetadata = <MetadataKeys extends string = TypicalMetadataKeys, T extends Record<string, any> = Record<string, any>>(
+  obj: T,
+  keys: MetadataKeys[] = (typicalMetadataKeys as any) as MetadataKeys[],
+): obj is HasMetadata<T, typeof keys[number]> => keys.every((key) => obj[key] !== undefined);

72ac7ac7b7f1055d0f7b61fe2fb5f62c98cd8738
Ulad Kasach <uladkasach@gmail.com>
2024-04-28 17:35:55 -0400

    feat(enum): expand isOfEnum with backwards compat assess and assure

--- src/checks/isOfEnum.test.ts
+++ src/checks/isOfEnum.test.ts
@@ -1,16 +1,35 @@
 import { createIsOfEnum } from './isOfEnum';
 
 describe('isOfEnum', () => {
-  it('can be used for custom enums correctly', () => {
-    enum Planet {
-      VENUS = 'VENUS',
-      EARTH = 'EARTH',
-      MARS = 'MARS',
-    }
+  enum Planet {
+    VENUS = 'VENUS',
+    EARTH = 'EARTH',
+    MARS = 'MARS',
+  }
+  it('can be used to assess correctly, default call', () => {
     const isPlanet = createIsOfEnum(Planet);
     const earthIsPlanet = isPlanet('EARTH');
     const plutoIsPlanet = isPlanet('PLUTO');
     expect(earthIsPlanet).toEqual(true);
     expect(plutoIsPlanet).toEqual(false);
+  });
+  it('can be used to assess correctly, named call', () => {
+    const isPlanet = createIsOfEnum(Planet);
+    const earthIsPlanet = isPlanet.assess('EARTH');
+    const plutoIsPlanet = isPlanet.assess('PLUTO');
+    expect(earthIsPlanet).toEqual(true);
+    expect(plutoIsPlanet).toEqual(false);
+  });
+  it('can be used to create type guarded branches', () => {
+    const isPlanet = createIsOfEnum(Planet);
+    const earth = 'EARTH';
+    if (isPlanet.assess(earth)) {
+      const home: Planet = earth; // no type error
+    }
+  });
+  it('can be used to create type asserted casts', () => {
+    const isPlanet = createIsOfEnum(Planet);
+    const earth = 'EARTH';
+    const home: Planet = isPlanet.assure(earth);
   });
 });
--- src/checks/isOfEnum.ts
+++ src/checks/isOfEnum.ts
@@ -1,3 +1,13 @@
+import { UnexpectedCodePathError } from '../utils/errors/UnexpectedCodePathError';
+
+type AssessMethod<
+  T extends { [index: string | number | symbol]: string | number },
+> = (
+  token: string | number | symbol,
+) => token is T[keyof T] & ((token: string | number | symbol) => boolean);
+type AssureMethod<
+  T extends { [index: string | number | symbol]: string | number },
+> = <I extends string | number | symbol>(input: I) => I & T[keyof T];
 /**
  * a generic fn that allows us to create type checks for enums. for example:
  * ```ts
@@ -11,15 +21,44 @@
  *   }
  *
  *   // define a type check for your enum
- *   const isPlanet = createIsOfEnum(Planet);
+ *   const isPlanet = createIsOfEnum.assess(Planet);
  *
- *   // use your new type check for a type guard
- *   if (!isPlanet(potentialPlanet)) throw new Error('is not a planet');
+ *   // use it to assess type for type guarded blocks
+ *   if (!isPlanet.assess(potentialPlanet)) throw new Error('is not a planet');
+ *
+ *   // use it to assure type for type guarded type casts
+ *   const planet: Planet = isPlanet.assure(potentialPlanet)
  * ```
  *
  * ref: https://stackoverflow.com/a/58278753/3068233
  */
-export const createIsOfEnum =
-  <T extends { [index: string | number | symbol]: string | number }>(e: T) =>
-  (token: string | number | symbol): token is T[keyof T] =>
+export const createIsOfEnum = <
+  T extends { [index: string | number | symbol]: string | number },
+>(
+  e: T,
+): {
+  assess: AssessMethod<T>;
+  assure: AssureMethod<T>;
+} & AssessMethod<T> => {
+  const assess = (token: string | number | symbol): token is T[keyof T] =>
     Object.values(e).includes(token as T[keyof T]);
+  const assure = <I extends string | number | symbol>(input: I) =>
+    assess(input)
+      ? input
+      : (() => {
+          throw new UnexpectedCodePathError(
+            'is not of DeclaredGoogleAdsCriterionUserInterestTaxonomy',
+          );
+        })();
+  const result = function (
+    ...input: Parameters<typeof assess>
+  ): ReturnType<typeof assess> {
+    return assess(...input);
+  } as AssessMethod<T> & {
+    assess: any;
+    assure: any;
+  };
+  result.assess = assess;
+  result.assure = assure;
+  return result;
+};

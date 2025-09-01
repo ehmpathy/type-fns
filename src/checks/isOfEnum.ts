4119498d0bc9291fd99207c97a415bc9dc9d96ce
Casey Brookes <58667871+caseybrookes@users.noreply.github.com>
2025-09-01 12:41:30 -0500

    fix(practs): bump to latest best; drop extra deps (#34)

--- src/checks/isOfEnum.ts
+++ src/checks/isOfEnum.ts
@@ -1,4 +1,4 @@
-import { withAssure } from '../wrappers/withAssure';
+import { AssessWithAssure, withAssure } from '../wrappers/withAssure';
 
 /**
  * a generic fn that allows us to create type checks for enums. for example:
@@ -28,7 +28,7 @@ export const createIsOfEnum = <
   T extends { [index: string | number | symbol]: string | number },
 >(
   e: T,
-) => {
+): AssessWithAssure<string | number | symbol, T[keyof T]> => {
   const check = (token: string | number | symbol): token is T[keyof T] =>
     Object.values(e).includes(token as T[keyof T]);
   return withAssure(check);
